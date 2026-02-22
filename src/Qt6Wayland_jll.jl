c0878832b5b5ec2b9c6d8a822ff7bf792ab52c64
jlbuild <juliabuildbot@gmail.com>
2026-02-22 13:25:43 +0000

    Qt6Wayland_jll build 6.10.2+0

--- src/Qt6Wayland_jll.jl
+++ src/Qt6Wayland_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("Qt6Wayland")
-JLLWrappers.@generate_main_file("Qt6Wayland", UUID("e99dba38-086e-5de3-a5b1-6e4c66e897c3"))
+JLLWrappers.@generate_main_file("Qt6Wayland", Base.UUID("e99dba38-086e-5de3-a5b1-6e4c66e897c3"))
 end  # module Qt6Wayland_jll
